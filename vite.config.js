42ed8e2fa080e45c93c62b8303a0483b286f93bb
Aldo Jonathan Ramirez Cortes <jonathanramirez419@gmail.com>
2023-05-17 02:21:36 -0600

    new adds

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://aldo-ramirez.github.io/ecommerce/",
+  base: "https://aldo-ramirez.github.io/ecommerceeee/",
 });
